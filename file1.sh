d12df3ad60f1b2639d232218f7d067c8d86b8570
Max <113039286+apelsin976@users.noreply.github.com>
2022-09-21 17:40:38 +0300

    Add files via upload

---
+++ file1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir FirstDown
+
+read -p "Input repository: " URL
+cd ./FirstDown
+git clone $URL
+
+mkdir ../Ffolder
+mv ./kyk/ ../Ffolder
+sudo chmod -R 600 ../Ffolder
+cd ..
+rm -rf ./FirstDown
+
+echo "Lab1 done"
